a0859ac970a16a4879f07794a5114f06400e5d9e
Robert Muwanga <robert.muwanga@outlook.com>
2018-10-30 18:29:49 +0300

    Update scrape-codenewbie.py
    Added feature in download method to prevent re-download of existing podcasts in current directory.

--- scrape-codenewbie.py
+++ scrape-codenewbie.py
@@ -1,25 +1,36 @@
 import requests
+import os
 import wget
 from bs4 import BeautifulSoup
 
 class Scrape:
     def __init__(self, url) :
-        self.url = urla
+        self.url = url
+        self.rss = None
+        self.podcasts = None
 
     def download(self, downloadList):
+        number_of_podcasts = len(downloadList)
+
         for i, podcast in enumerate(downloadList):
-            print('\n## Downloading {} of {}'.format(i+1, len(downloadList)))
-            wget.download(podcast)
+            if(os.path.exists(podcast.split('/')[-1])) :
+                print('\n## Skipping {} of {}. File exists.'.format(i+1, number_of_podcasts))
+            else:
+                print('\n## Downloading {} of {}'.format(i+1, number_of_podcasts))
+                wget.download(podcast)
+                print('## Done. Downloaded {} of {}'.format(i+1, number_of_podcasts))
 
     def scrape(self) :
-        codenewbie = BeautifulSoup(requests.get(url).text, features='lxml')
-        podcasts = [x['url'] for x in codenewbie.find_all(name='enclosure')]
+        self.rss = BeautifulSoup(requests.get(url).text, features='lxml')
+        self.podcasts = [x['url'] for x in self.rss.find_all(name='enclosure')]
         
-        return podcasts
+        return self.podcasts
 
 if __name__ == '__main__':
     url = 'http://feeds.codenewbie.org/cnpodcast.xml'
     codenewbie = Scrape(url)
-    codenewbie.download(codenewbie.scrape()[:2])
+    
+    print('\nDownloading CodeNewbie podcasts into current folder ....')
+    codenewbie.download(codenewbie.scrape())
 
     print('\nDone scraping and downloading CodeNewbie podcasts.')
